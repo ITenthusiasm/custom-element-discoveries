8c972bfa5552aa52d3a8d9bb7e4c8b978d049af3
ITenthusiasm <47364027+ITenthusiasm@users.noreply.github.com>
2025-10-07 22:25:15 -0500

    chore: Improve Svelte TypeScript Types for Combobox component
    - Adds the `filterchange` event handler to the `ComboboxField`
    - Removes some code that didn't seem necessary

--- library-tests/form-observer/svelte/svelte.d.ts
+++ library-tests/form-observer/svelte/svelte.d.ts
@@ -1,25 +1,21 @@
+import type { EventHandler } from "svelte/elements";
 import type { SelectEnhancer, ComboboxField, ComboboxListbox, ComboboxOption } from "../../../src/Combobox/index.js";
 
-/* https://svelte.dev/docs/svelte/typescript#Enhancing-built-in-DOM-types */
 declare module "svelte/elements" {
-  export interface SvelteHTMLElements {
+  interface SvelteHTMLElements {
     "select-enhancer": HTMLSelectEnhancerAttributes;
     "combobox-field": HTMLComboboxFieldAttributes<ComboboxField>;
     "combobox-listbox": HTMLAttributes<ComboboxListbox>;
     "combobox-option": HTMLComboboxOptionAttributes;
   }
 
-  // NOTE: We need this line so that the TypeScript compiler can recognize the `HTMLAttributes` type.
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unused-vars
-  export interface HTMLAttributes<T> {}
-
-  export interface HTMLSelectEnhancerAttributes extends HTMLAttributes<SelectEnhancer> {
+  interface HTMLSelectEnhancerAttributes extends HTMLAttributes<SelectEnhancer> {
     comboboxtag?: SelectEnhancer["comboboxTag"] | null;
     listboxtag?: SelectEnhancer["listboxTag"] | null;
     optiontag?: SelectEnhancer["optionTag"] | null;
   }
 
-  export interface HTMLComboboxFieldAttributes<T extends EventTarget> extends HTMLAttributes<T> {
+  interface HTMLComboboxFieldAttributes<T extends EventTarget> extends HTMLAttributes<T> {
     disabled?: ComboboxField["disabled"] | null;
     filter?: ComboboxField["filter"] | null;
     filtermethod?: ComboboxField["filterMethod"] | null;
@@ -29,17 +25,19 @@ declare module "svelte/elements" {
     required?: ComboboxField["required"] | null;
     valueis?: ComboboxField["valueIs"] | null;
     valuemissingerror?: ComboboxField["valueMissingError"] | null;
+
+    "on:filterchange"?: EventHandler<Event, T> | null;
+    onfilterchange?: EventHandler<Event, T> | null;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type -- This is required to support ComboboxField attrs
-  export interface HTMLSelectAttributes extends HTMLComboboxFieldAttributes<HTMLSelectElement> {}
+  interface HTMLSelectAttributes
+    extends Omit<HTMLComboboxFieldAttributes<HTMLSelectElement>, "onfilterchange" | "on:filterchange"> {}
 
-  export interface HTMLComboboxOptionAttributes extends HTMLAttributes<ComboboxOption> {
-    defaultSelected?: ComboboxOption["defaultSelected"];
+  interface HTMLComboboxOptionAttributes extends HTMLAttributes<ComboboxOption> {
+    defaultSelected?: ComboboxOption["defaultSelected"] | null;
     disabled?: ComboboxOption["disabled"] | null;
     selected?: ComboboxOption["selected"] | null;
     value?: ComboboxOption["value"] | null;
   }
 }
-
-export {}; // Ensure this is not an Ambient Module. Otherwise, original Svelte types will be overridden instead of augmented.
